452f7d3a5305bdb521e10df0befda716044cf186
Foxrim <Flavien085@gmail.com>
2024-11-02 19:34:02 +0100

    [feat] : Fin de l'exo

---
+++ src/components/App.tsx
@@ -0,0 +1,44 @@
+import '../styles/App.css'
+import EmployeeCard from './EmployeeCard'
+
+import { useEffect, useState } from 'react';
+
+const sampleEmployee = {
+  name: {
+    first: "Charlie",
+    last: "Thompson",
+  },
+  email: "charlie.thompson@example.com",
+  picture: {
+    medium: "https://randomuser.me/api/portraits/med/men/40.jpg",
+  },
+};
+
+function App() {
+  const [employee, setEmployee] = useState(sampleEmployee);
+
+  useEffect(() => {
+    fetch('http://localhost:3310/api/employees')
+      .then(response => response.json())
+      .then(data => {
+        setEmployee(data.results[0]);
+      });
+  }, []);
+
+  const getEmployee = () => {
+    fetch("https://randomuser.me/api?nat=en")
+      .then((response) => response.json())
+      .then((data) => {
+        setEmployee(data.results[0]);
+      });
+  };
+
+  return (
+    <div className='App'>
+      <EmployeeCard employee={employee} />
+      <button type="button" onClick={getEmployee}>Get employee</button>
+    </div>
+  );
+}
+
+export default App
